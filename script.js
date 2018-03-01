953144c6862edbac0f2bd229013b2630e38983d9
yyaroshevich <yyaroshevich@exadel.com>
2018-03-01 22:35:18 +0300

    new result

--- script.js
+++ script.js
@@ -1,25 +1,109 @@
-var fs = require('fs');
+const fs = require('fs');
 
-var contents = fs.readFileSync('a_example.in', 'utf8');
+// const contents = fs.readFileSync('b_should_be_easy.in', 'utf8');
+const contents = fs.readFileSync('c_no_hurry.in', 'utf8');
+// const contents = fs.readFileSync('d_metropolis.in', 'utf8');
+// const contents = fs.readFileSync('e_high_bonus.in', 'utf8');
 
 const lines = contents.split('\n');
-const  [R, C, F, N, B, T] = lines[0].split(' ');
+const [R, C, F, N, B, T] = lines[0].split(' ').map(line => +line);
 
 lines.shift();
 lines.pop();
 
-const rides = lines.map((line) => {
+const rides = lines.map((line, ind) => {
 	line = line.split(' ');
 	return {
-		a: line[0],
-		b: line[1],
-		x: line[2],
-		y: line[3],
-		s: line[4],
-		f: line[5]
+		a: +line[0],
+		b: +line[1],
+		x: +line[2],
+		y: +line[3],
+		s: +line[4],
+		f: +line[5],
+		ind: ind
 	};
 });
 
+const answers = [];
 
+let totalCost = 0;
 
-console.log();
+const cars = [];
+for (let i = 0; i < F; i++) {
+	cars.push({
+		xCur: 0,
+		yCur: 0,
+		tCur: 0,
+		ind: i
+	});
+
+	answers.push([]);
+}
+
+do {
+	let max = {
+		speed: 0,
+		cost: 0
+	};
+
+	rides.forEach((ride, ind) => {
+		cars.forEach((car) => {
+			const res = cost(car, ride);
+			if (res.speed > max.speed) {
+				max = res;
+			}
+		});
+	});
+
+	if (max.cost === 0) {
+		break;
+	}
+
+	max.car.tCur += max.time;
+	max.car.xCur = max.ride.x;
+	max.car.yCur = max.ride.y;
+
+	totalCost += max.cost;
+
+	answers[max.car.ind].push(max.ride.ind);
+	rides.splice(rides.indexOf(max.ride), 1);
+	console.log(rides.length);
+} while (rides.length);
+
+fs.writeFileSync('c_no_hurry.out', answers.map((ans) => {
+	return `${ans.length} ${ans.join(' ')}`;
+}).join('\n'), 'utf8');
+
+function cost(car, ride) {
+	let cost = 0;
+	let time = Math.abs(ride.a - car.xCur) + Math.abs(ride.b - car.yCur);
+
+	if (time + car.tCur <= ride.s) {
+		time += ride.s - time - car.tCur;
+		cost += B;
+	}
+
+	const path = Math.abs(ride.a - ride.x) + Math.abs(ride.b - ride.y);
+	time += path;
+	cost += path;
+
+	if (time + car.tCur >= ride.f) {
+		return {
+			time: time,
+			cost: 0,
+			ride: ride,
+			car: car,
+			speed: 0
+		};
+	}
+
+	return {
+		time: time,
+		cost: cost,
+		ride: ride,
+		car: car,
+		speed: cost / time
+	};
+}
+
+console.log(totalCost);
