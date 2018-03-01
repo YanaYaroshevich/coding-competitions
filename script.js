661155059e772c0c500af78a69cd9c97ad13f6e8
yyaroshevich <yyaroshevich@exadel.com>
2018-03-01 21:04:46 +0300

    read file

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ var fs = require('fs');
 
 var contents = fs.readFileSync('a_example.in', 'utf8');
 
-console.log(contents);
+const lines = contents.split('\n');
+const  [R, C, F, N, B, T] = lines[0].split(' ');
+
+lines.shift();
+lines.pop();
+
+const rides = lines.map((line) => {
+	line = line.split(' ');
+	return {
+		a: line[0],
+		b: line[1],
+		x: line[2],
+		y: line[3],
+		s: line[4],
+		f: line[5]
+	};
+});
+
+
+
+console.log();
