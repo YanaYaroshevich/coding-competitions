bf0d220f2ccd2c9f2e201f67a1a2bdc811bf425d
YanaYaroshevich <yanay@wix.com>
2022-12-03 09:03:37 +0200

    advent2022: dec_3 task_2

---
+++ 2022/adventofcode/dec_3/task2.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const contents = fs.readFileSync('input.txt', 'utf8');
+const lines = contents.split('\n');
+
+const badges = [];
+let itemsMap = {};
+
+
+for (let i = 0; i < lines.length; i += 3) {
+    for (let j = 0; j < 3; j++) {
+        const line = lines[i + j];
+        const tmpItemMap = {};
+        for (let k = 0; k < line.length; k++) {
+            const item = line[k];
+            if (!tmpItemMap[item]) {
+                tmpItemMap[item] = true;
+                itemsMap[item] = itemsMap[item] ? itemsMap[item] + 1: 1;
+            }
+        }
+    }
+    badges.push(Object.keys(itemsMap).find((key) => itemsMap[key] === 3));
+    itemsMap = {};
+}
+
+console.log(badges);
+
+
+const aCharCode = 'a'.charCodeAt(0);
+const ACharCode = 'A'.charCodeAt(0);
+
+const prioritiesSum = badges.reduce((acc, it) => {
+    const code = it.charCodeAt(0);
+    if (code >= aCharCode) {
+        acc += code - aCharCode + 1;
+    } else {
+        acc += code - ACharCode + 27;
+    }
+
+    return acc;
+}, 0);
+
+console.log(prioritiesSum);
