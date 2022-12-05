d29a9d0bf4f5ce8dd98ae9cd6739386998788aa3
YanaYaroshevich <yanay@wix.com>
2022-12-05 09:58:17 +0200

    advent2022: dec_5 task_1, task_2

---
+++ 2022/adventofcode/dec_5/task1.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const contents = fs.readFileSync('input.txt', 'utf8');
+const lines = contents.split('\n');
+
+let j = 0;
+const stacks = [];
+
+for (let i = 0; i < lines.length; i++) {
+    let newLine = lines[i];
+
+    if (isNaN(newLine)) {
+        for (let k = 0; k < newLine.length; k++) {
+            if (newLine[k] === '[') {
+                const ind = Math.floor(k / 4);
+                stacks[ind] = stacks[ind] ? [newLine[k + 1], ...stacks[ind]] : [newLine[k + 1]];
+                k += 3;
+            }
+        }
+    } else {
+        j = i + 1;
+        break;
+    }
+}
+
+for (let i = j; i < lines.length; i++) {
+    const [q, from, to] = lines[i].match(/\d+/g);
+    for (let k = 0; k < q; k++) {
+        stacks[to - 1].push(stacks[from - 1].pop());
+    }
+}
+
+console.log(stacks.map((el) => el.pop()).join(''));
