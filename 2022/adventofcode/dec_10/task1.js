61be02581fef87ba7d6c0c509a3bc87a2057361a
YanaYaroshevich <yanay@wix.com>
2022-12-10 08:26:17 +0200

    advent2022: dec_10

---
+++ 2022/adventofcode/dec_10/task1.js
@@ -0,0 +1,49 @@
+/*
+*
+
+* * */
+
+const fs = require('fs');
+
+const contents = fs.readFileSync('input.txt', 'utf8');
+const lines = contents.split('\n');
+
+const neededMoves = [20, 60, 100, 140, 180, 220];
+
+let x = 1;
+let i = 0;
+let powerSum = 0;
+let line;
+
+while (true) {
+  if (!lines.length) {
+    break;
+  }
+
+  if (neededMoves.includes(i + 1)) {
+    powerSum += (i + 1) * x;
+  }
+
+  if (!line) {
+    line = lines.shift();
+  }
+
+  if (line === 'noop') {
+    i += 1;
+    line = null;
+    continue;
+  }
+
+  if (line !== 'noop') {
+    const [, num] = line.split(' ');
+    i++;
+    if (neededMoves.includes(i + 1)) {
+      powerSum += (i + 1) * x;
+    }
+    i++;
+    x += Number(num);
+    line = null;
+  }
+}
+
+console.log(powerSum);
---
+++ 2022/adventofcode/dec_10/task2.js
@@ -0,0 +1,52 @@
+/*
+*
+
+* * */
+
+const fs = require('fs');
+
+const contents = fs.readFileSync('input.txt', 'utf8');
+const lines = contents.split('\n');
+
+let x = 1;
+
+const image = new Array(6);
+for (let k = 0; k < image.length; k++) {
+    image[k] = '';
+}
+
+let imageIndex = 0;
+
+const drawSymbol = () => {
+    const symbInd = imageIndex % 40;
+    image[Math.floor(imageIndex / 40)] += Math.abs(symbInd - x) <= 1 ? '#' : '.';
+    imageIndex++;
+};
+
+let line;
+
+while (true) {
+    if (!lines.length) {
+        break;
+    }
+
+    if (!line) {
+        line = lines.shift();
+    }
+
+    if (line === 'noop') {
+        line = null;
+        drawSymbol();
+        continue;
+    }
+
+    if (line !== 'noop') {
+        const [, num] = line.split(' ');
+        drawSymbol();
+        drawSymbol();
+        x += Number(num);
+        line = null;
+    }
+}
+
+console.log(image);
