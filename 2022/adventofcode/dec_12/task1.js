ea3e9ddee576c8f87a6caf41401d23a245542632
YanaYaroshevich <yanay@wix.com>
2022-12-12 09:45:33 +0200

    advent2022: dec_12 task_1, task_2

---
+++ 2022/adventofcode/dec_12/task1.js
@@ -0,0 +1,63 @@
+/*
+*
+
+* * */
+
+const fs = require('fs');
+
+const contents = fs.readFileSync('input.txt', 'utf8');
+const lines = contents.split('\n');
+
+let start;
+let end;
+const matr = [];
+
+for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+
+    if (line.indexOf('S') !== -1) {
+        start = [i, line.indexOf('S')];
+    }
+
+    if (line.indexOf('E') !== -1) {
+        end = [i, line.indexOf('E')];
+    }
+
+    matr.push([...line.split('').map((el) => ({val: el === 'S' ? 'a'.charCodeAt(0) : (el === 'E' ? 'z'.charCodeAt(0) : el.charCodeAt(0)), visited: false}))]);
+}
+
+const stack = [{point: start, moveInd: 0}];
+let lastMoveInfo;
+
+while(stack.length) {
+    const move = stack.shift();
+    const [x, y] = move.point;
+    const moveInd = move.moveInd;
+
+    if (x === end[0] && y === end[1]) {
+        lastMoveInfo = move;
+        break;
+    }
+
+    if (x > 0 && matr[x-1][y].val <= matr[x][y].val + 1 && !matr[x-1][y].visited) {
+        stack.push({ point: [x-1, y], moveInd: moveInd + 1 });
+        matr[x-1][y].visited = true;
+    }
+
+    if (x < matr.length - 1 && matr[x+1][y].val <= matr[x][y].val + 1 && !matr[x+1][y].visited) {
+        stack.push({ point: [x+1, y], moveInd: moveInd + 1 });
+        matr[x+1][y].visited = true;
+    }
+
+    if (y > 0 && matr[x][y-1].val <= matr[x][y].val + 1 && !matr[x][y-1].visited) {
+        stack.push({ point: [x, y-1], moveInd: moveInd + 1 });
+        matr[x][y-1].visited = true;
+    }
+
+    if (y < matr[0].length - 1 && matr[x][y+1].val <= matr[x][y].val + 1 && !matr[x][y+1].visited) {
+        stack.push({ point: [x, y+1], moveInd: moveInd + 1 });
+        matr[x][y+1].visited = true;
+    }
+}
+
+console.log(lastMoveInfo);
