23b0216ac85dd0da3f56881101eaa7e84147db93
YanaYaroshevich <yanay@wix.com>
2022-12-14 12:23:08 +0200

    advent2022: dec_14 task_1

--- 2022/adventofcode/dec_14/task1.js
+++ 2022/adventofcode/dec_14/task1.js
@@ -19,24 +19,24 @@ for (let i = 0; i < lines.length; i++) {
         const prevPair = pairs[j - 1];
 
         if (prevPair[0] !== pair[0]) {
-            let start = prevPair[0] > pair[0] ? pair[0] : prevPair[0];
-            let end = prevPair[0] > pair[0] ? prevPair[0] : pair[0];
+            let start = +prevPair[0] > +pair[0] ? +pair[0] : +prevPair[0];
+            let end = +prevPair[0] > +pair[0] ? +prevPair[0] : +pair[0];
 
-            if (pair[1] > max) {
-                max = pair[1];
+            if (+pair[1] > max) {
+                max = +pair[1];
             }
-           for (let k = start; k <= end; k++) {
-               if (!matr[k]) {
-                   matr[k] = new Array(1000).fill('.');
-               }
-               matr[k][pair[1]] = '#';
-           }
-           continue;
+            for (let k = start; k <= end; k++) {
+                if (!matr[k]) {
+                    matr[k] = new Array(1000).fill('.');
+                }
+                matr[k][+pair[1]] = '#';
+            }
+            continue;
         }
 
         if (prevPair[1] !== pair[1]) {
-            let start = prevPair[1] > pair[1] ? pair[1] : prevPair[1];
-            let end = prevPair[1] > pair[1] ? prevPair[1] : pair[1];
+            let start = +prevPair[1] > +pair[1] ? +pair[1] : +prevPair[1];
+            let end = +prevPair[1] > +pair[1] ? +prevPair[1] : +pair[1];
             if (end > max) {
                 max = end;
             }
@@ -52,18 +52,34 @@ for (let i = 0; i < lines.length; i++) {
 }
 
 const logMatr = () => {
+    let arr = [];
     let output = '';
-    for (let j = 0; j < max; j++) {
+    for (let j = 0; j <= max; j++) {
+        output += `${j < 10 ? '00' : j < 100 ? '0' : ''}${j}`;
         for (let i = 0; i < matr.length; i++) {
             if (!matr[i]) {
                 continue;
+            }
+            if(j === 0) {
+                arr.push(`${i < 10 ? '00' : i < 100 ? '0' : ''}${i}`.split(''));
             }
             output += matr[i][j];
         }
         output += '\n';
     }
+    let str = '';
+    for (let j = 0; j < 3; j++) {
+        str += '   ';
+        for (let i = 0; i < arr.length; i++) {
+            str += arr[i][j];
+        }
+        str += '\n';
+    }
+    output = str + output;
     console.log(output);
 }
+
+logMatr();
 
 let sandX;
 let sandY;
@@ -73,7 +89,7 @@ for (let i = 1;; i++) {
     sandY = 0;
 
     while (true) {
-        if (sandY > max) {
+        if (sandY >= max) {
             logMatr();
             console.log(i);
 
