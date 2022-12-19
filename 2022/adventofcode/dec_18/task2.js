dca41f37f05bb4014de727cd2e2ef9b5e30e24ae
YanaYaroshevich <yanay@wix.com>
2022-12-19 18:10:45 +0200

    advent2022: dec_18 task_2

---
+++ 2022/adventofcode/dec_18/task2.js
@@ -0,0 +1,85 @@
+/*
+*
+
+* * */
+
+const fs = require("fs");
+
+const contents = fs.readFileSync("input.txt", "utf8");
+const lines = contents.split("\n");
+
+const cubesObj = {};
+
+let xMax = 0;
+let xMin = 0;
+let yMax = 0;
+let yMin = 0;
+let zMax = 0;
+let zMin = 0;
+
+lines.forEach((line) => {
+  const [x, y, z] = line.split(",").map(Number);
+  cubesObj[line] = { x, y, z };
+  xMax = Math.max(xMax, x);
+  xMin = Math.min(xMin, x);
+  yMax = Math.max(yMax, y);
+  yMin = Math.min(yMin, y);
+  zMax = Math.max(zMax, z);
+  zMin = Math.min(zMin, z);
+});
+
+Object.keys(cubesObj).forEach((cube) => {
+  const { x, y, z } = cubesObj[cube];
+  const neighbors = [
+    `${x - 1},${y},${z}`,
+    `${x + 1},${y},${z}`,
+    `${x},${y - 1},${z}`,
+    `${x},${y + 1},${z}`,
+    `${x},${y},${z - 1}`,
+    `${x},${y},${z + 1}`,
+  ];
+});
+
+const start = { x: xMin - 1, y: yMin, z: zMin };
+console.log(start);
+
+const markedEmptyCubes = { [`${start.x},${start.y},${start.z}`]: true };
+const queue = [start];
+let neigbourCount = 0;
+
+while (queue.length) {
+  const { x, y, z } = queue.pop();
+
+  const neighbors = [
+    `${x - 1},${y},${z}`,
+    `${x + 1},${y},${z}`,
+    `${x},${y - 1},${z}`,
+    `${x},${y + 1},${z}`,
+    `${x},${y},${z - 1}`,
+    `${x},${y},${z + 1}`,
+  ];
+
+  neighbors.forEach((neig) => {
+    if (!cubesObj[neig] && !markedEmptyCubes[neig]) {
+      const [x, y, z] = neig.split(",").map(Number);
+      if (
+        x > xMax + 1 ||
+        x < xMin - 1 ||
+        y > yMax + 1 ||
+        y < yMin - 1 ||
+        z > zMax + 1 ||
+        z < zMin - 1
+      ) {
+        return;
+      }
+      markedEmptyCubes[neig] = true;
+      queue.push({ x, y, z });
+    } else if (cubesObj[neig]) {
+      neigbourCount++;
+    }
+  });
+}
+
+console.log(cubesObj);
+
+console.log(neigbourCount);
