cb14368cd0b769e4cbf36a164c5190ce30a2f972
yanay <yanay@wix.com>
2023-11-29 18:02:44 +0100

    preparations for advent of code 2023 <3

---
+++ 2023/adventofcode/draft/task1.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+
+const contents = fs.readFileSync("input1.txt", "utf8");
+const lines = contents.split("\n");
+
---
+++ 2023/adventofcode/draft/task2.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+
+const contents = fs.readFileSync("input2.txt", "utf8");
+const lines = contents.split("\n");
