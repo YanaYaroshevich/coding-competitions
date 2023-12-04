715cd64d4546034b15577a45ec824e628dac5833
yanay <yanay@wix.com>
2023-12-04 09:43:11 +0100

    adventofcode: Dec 4

---
+++ 2023/adventofcode/dec_4/task1.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const contents = fs.readFileSync('input1.txt', 'utf8');
+const lines = contents.split('\n');
+
+let allPoints = 0;
+lines.forEach((line, ind) => {
+  const [winNums, myNums] = line
+    .replace(/Card\s+\d+: /, '')
+    .split(' | ')
+    .map((numsStr) => numsStr.split(/\s+/).map((s) => +s).sort((a, b) => a - b));
+
+  console.log(winNums, myNums);
+  const resNums = [];
+
+  let i = 0;
+  let j = 0;
+  while (i < myNums.length && j < winNums.length) {
+    if (myNums[i] > winNums[j]) {
+      j++;
+      continue;
+    }
+    if (myNums[i] < winNums[j]) {
+      i++;
+      continue;
+    }
+    if (myNums[i] === winNums[j]) {
+      resNums.push(myNums[i]);
+      i++;
+      j++;
+    }
+  }
+
+  const finalPoints = resNums.length === 0 ? 0 : 2 ** (resNums.length - 1);
+  allPoints += finalPoints;
+});
+
+console.log(allPoints);
