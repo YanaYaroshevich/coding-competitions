fa34fec48fa4d3095aed6159d0cd4b3ac5b974fc
Yana Yaroshevich <yanay@wix.com>
2024-12-02 15:44:34 +0100

    dec 2 2024

--- 2024/adventofcode/dec_2/task2.js
+++ 2024/adventofcode/dec_2/task2.js
@@ -3,46 +3,43 @@ const fs = require('fs');
 const contents = fs.readFileSync('input.txt', 'utf8');
 const lines = contents.split('\n');
 
-const run = (levels, isRemoved) => {
+const run = (levels) => {
     let isIncreasing = levels.length > 1 ? levels[1] > levels[0] : false;
     for (let i = 1; i < levels.length; i++) {
         if (isIncreasing) {
-            if (levels[i] < levels[i - 1] || levels[i] - levels[i - 1] > 3 || levels[i] - levels[i - 1] === 0) {
-                if (isRemoved) {
-                    return false;
-                }
-                if (run(levels.slice(0, i).concat(levels.slice(i + 1)), true)) {
-                    return true;
-                }
-                if (run(levels.slice(0, i - 1).concat(levels.slice(i)), true)) {
-                    return true;
-                }
+            if (levels[i] < levels[i - 1]) {
+                return false;
+            }
+            if (levels[i] - levels[i - 1] > 3 || levels[i] - levels[i - 1] === 0) {
                 return false;
             }
         }
         if (!isIncreasing) {
-            if (levels[i] > levels[i - 1] || levels[i - 1] - levels[i] > 3 || levels[i - 1] - levels[i] === 0) {
-                if (isRemoved) {
-                    return false;
-                }
-                if (run(levels.slice(0, i).concat(levels.slice(i + 1)), true)) {
-                    return true;
-                }
-                if (run(levels.slice(0, i - 1).concat(levels.slice(i)), true)) {
-                    return true;
-                }
+            if (levels[i] > levels[i - 1]) {
+                return false;
+            }
+            if (levels[i - 1] - levels[i] > 3 || levels[i - 1] - levels[i] === 0) {
                 return false;
             }
         }
-    }
 
+    }
     return true;
 }
 
 const saveCount = lines.reduce((prev, cur) => {
     const levels = cur.split(/\s+/).map(Number);
 
-    return run(levels, false) ? prev + 1 : prev;
+    if (run(levels)) {
+        return prev + 1;
+    }
+
+    for (let i = 0; i < levels.length; i++) {
+        if (run(levels.slice(0, i).concat(levels.slice(i + 1)))) {
+            return prev + 1;
+        }
+    }
+    return prev;
 }, 0);
 
 console.log(saveCount);
